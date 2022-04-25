a93fb7fc652fae33cd3863f468cc2a026f2cb851
Luke Lozenski <ljlozenski@laspezia.seas.wustl.edu>
2022-04-25 13:26:13 -0500

    Added Copyright statements

--- applications/full_run.sh
+++ applications/full_run.sh
@@ -1,3 +1,15 @@
+# Copyright (c) 2022, Washington University in St. Louis.
+#
+# All Rights reserved.
+# See file COPYRIGHT for details.
+#
+# This file is part of the Neural Field CRT Dynamic Imaging Library. For more information and source code
+# availability see https://github.com/precise-wustl/nf_crt_dynamic_imaging.
+#
+# Neural Field CRT Dynamic Imaging is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License (as published by the Free
+# Software Foundation) version 3.0 dated June 2007.
+
 #Runs representatoin or embedding expirement
 python representaiton.py
 python create_animations.py --out_folder Representation_Results/
--- applications/variable_noise_level.sh
+++ applications/variable_noise_level.sh
@@ -1,3 +1,15 @@
+# Copyright (c) 2022, Washington University in St. Louis.
+#
+# All Rights reserved.
+# See file COPYRIGHT for details.
+#
+# This file is part of the Neural Field CRT Dynamic Imaging Library. For more information and source code
+# availability see https://github.com/precise-wustl/nf_crt_dynamic_imaging.
+#
+# Neural Field CRT Dynamic Imaging is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License (as published by the Free
+# Software Foundation) version 3.0 dated June 2007.
+
 #Runs expirement with fixed number of views per frame and varying noise level
 export OPENBLAS_NUM_THREADS=1
 export OPENMP_NUM_THREADS=1
